62053fe29168cd703c9fd4ee462f3e817bdebbcd
callumtw <callumtw@googlemail.com>
2020-03-03 11:24:36 +0000

    Revert "Attempts to stop it from requesting web stuff"
    This reverts commit 2eaefbdf2b94a4378fc61505ce6a5d4532c9b739.

--- test/fixtures/dotnetcore-2.1/Program.cs
+++ test/fixtures/dotnetcore-2.1/Program.cs
@@ -14,12 +14,12 @@ namespace dotnetcore_2_1
     {
         public static void Main(string[] args)
         {
-            //BuildWebHost(args).Run();
+            BuildWebHost(args).Run();
         }
 
-        /*public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();*/
+                .Build();
     }
 }
